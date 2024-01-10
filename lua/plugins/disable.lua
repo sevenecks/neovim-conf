2cc928d559bd58b42a2579f730a1c1d577544a22
Seven Ecks <sevenecks@gmail.com>
2024-01-10 12:24:51 -0500

    disabled autocomplete plugin

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,8 @@
+return {
+  -- disable friendly-snippets
+  -- { "rafamadriz/friendly-snippets", enabled = false },
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+  -- this disables the plugin which does autocomplete popups
+  -- which I find very freaking annoying
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+}
